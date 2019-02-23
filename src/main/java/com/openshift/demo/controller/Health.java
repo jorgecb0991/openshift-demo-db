7d7f11e7ccf3d024b8c322ccce1cc2956ac4ea1e
Marco Benites <marco.benitese@gmail.com>
2019-02-23 00:50:45 -0500

    Se configura cadena de conexion para conexion con POD mysql en OpenShift mediante el IP-CLUSTER definido para su servicio

--- src/main/java/com/openshift/demo/controller/Health.java
+++ src/main/java/com/openshift/demo/controller/Health.java
@@ -1,5 +1,7 @@
 package com.openshift.demo.controller;
 
+import java.util.Iterator;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.openshift.demo.entity.Usuario;
 import com.openshift.demo.repositorio.UsuarioRepository;
 
 @RestController
@@ -17,9 +20,21 @@ public class Health {
  
     @Autowired
     UsuarioRepository systemRepository;
+    
 	@RequestMapping(value="/salud", method=RequestMethod.GET)
 	public String login() {
-		systemRepository.findAll();
-		return "Hola Mundo openshift!!";
+		
+		Iterable<Usuario> usuarioIterator = systemRepository.findAll();
+		
+		Iterator<Usuario> iterator = usuarioIterator.iterator();
+		
+		int contador = 0;
+		if (iterator.hasNext()) {
+//			Usuario user = iterator.next();
+			contador++;
+		}
+		
+		
+		return "Hola Mundo openshift!! Se encontraron " + contador + " usuarios desde la BD.";
 	}
 }
